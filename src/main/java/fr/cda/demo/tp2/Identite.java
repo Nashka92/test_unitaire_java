8d4cd27b43227c567c764448ec85cbc2c7f8ae29
Anna RAACH <annagraph92@gmail.com>
2024-01-04 10:31:26 +0100

    feat(tp2): classe identite gestion personnel

--- src/main/java/fr/cda/demo/tp2/Identite.java
+++ src/main/java/fr/cda/demo/tp2/Identite.java
@@ -1,2 +1,34 @@
-package fr.cda.demo.tp2;public class Identite {
+package fr.cda.demo.tp2;
+
+
+public class Identite {
+    private String nom;
+    private String prenom;
+    private Adresse adresse;
+
+    public Identite(String nom, String prenom, String voie, String rue, String cp, String ville, String pays) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = new Adresse(voie, rue, cp, ville, pays);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
 }
