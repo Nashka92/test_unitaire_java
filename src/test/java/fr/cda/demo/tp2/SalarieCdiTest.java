60015760a095a92efb6080645606bfafca8a21ae
Anna RAACH <annagraph92@gmail.com>
2024-01-05 00:11:30 +0100

    docs(tp2): documentation pour SalarieCdiTest 📝

--- src/test/java/fr/cda/demo/tp2/SalarieCdiTest.java
+++ src/test/java/fr/cda/demo/tp2/SalarieCdiTest.java
@@ -13,20 +13,32 @@ class SalarieCdiTest {
     private Identite identiteMock;
     private Salaire salaireMock;
 
+    /**
+     * Annotation BeforeEach exécutée avant chaque test unitaire
+     * Il prépare l'environnement de test avant d'exécuter chaque test individuel
+     */
     @BeforeEach
     public void setUp() {
+
+         // Ici on utilise Mockito pour créer un simulacre.
+         // Ce mock sera utilisé pour simuler l'objet Identite et Salaire dans les tests
         identiteMock = mock(Identite.class);
         when(identiteMock.getIdentite()).thenReturn("Bernard", "Lavillier");
 
         salaireMock = mock(Salaire.class);
         when(salaireMock.getSalaire()).thenReturn(151.67F);
 
+        // Crée une instance de SalarieCdi en passant les mocks salaireMock et identiteMock
         salarieCdi = new SalarieCdi(
                 salaireMock,
                 identiteMock
         );
     }
 
+    /**
+     * Ce test testDemenager vérifie la méthode demenager()
+     * et elle met à jour la nouvelle adresse
+     */
     @Test
     @DisplayName("Test le salarié déménage et change d'adresse")
     public void testDemenager() {
@@ -42,6 +54,10 @@ class SalarieCdiTest {
         ).setAdresse(adresseMock);
     }
 
+    /**
+     * testTravaillerHeures vérifie que la méthode travailler() fonctionne
+     * pour un nombre d'heures de travail
+     */
     @Test
     @DisplayName("Test le salarié travaille et gagne son salaire")
     public void testTravaillerHeures() {
@@ -53,10 +69,15 @@ class SalarieCdiTest {
         when(salaireMock.payer(heures)).thenReturn(salaireAttendu);
 
         salarieCdi.travailler(heures);
-
+        // Vérifier que certaines interactions ont eu lieu avec l'objet mocké
+        // On spécifie le nombre de fois que l'interaction attendue doit avoir eu lieu, ici times 1
         verify(salaireMock, times(1)).payer(heures);
     }
 
+    /**
+     * testAugmenteSalaire vérifie si la méthode augmenter()
+     * calcule correctement le nouveau taux horaire
+     */
     @Test
     @DisplayName("Test l'augmentation du salaire")
     public void testAugmenteSalaire() {
