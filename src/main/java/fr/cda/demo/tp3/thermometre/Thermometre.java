fe99914d19a5c4ea93dd7e86b60d3cad34d32768
Anna RAACH <annagraph92@gmail.com>
2024-01-05 16:29:45 +0100

    test(tp3): psartek frero les test thermometre retourne bien 1 et 4 ✅

--- src/main/java/fr/cda/demo/tp3/thermometre/Thermometre.java
+++ src/main/java/fr/cda/demo/tp3/thermometre/Thermometre.java
@@ -1,7 +1,24 @@
 package fr.cda.demo.tp3.thermometre;
 
 public class Thermometre {
-    public int thermometreEgalite(int [] temperatures){
-        return 1;
+    public int thermometreEgalite(int[] temperatures) {
+
+        if (temperatures == null) {
+            return 0;
+        }
+
+        // Initialisation de la valeur minimum
+        int minVal = temperatures[0];
+
+        // Parcours le tableau temperatures pour trouver la valeur minimale
+        for (int index = 1; index < temperatures.length; index++) {
+            if (temperatures[index] < minVal) {
+                minVal = temperatures[index];
+            }
+        }
+
+        // Retournez la valeur minimale
+        return minVal;
     }
 }
+
--- src/test/java/fr/cda/demo/tp3/thermometre/ThermometreTest.java
+++ src/test/java/fr/cda/demo/tp3/thermometre/ThermometreTest.java
@@ -9,14 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ThermometreTest {
 
+    /**
+     * On retourne le plus petit du tableau
+     */
     @Test
-    public void thermometre_egalite_Positive_retourne_1(){
+    public void thermometre_egalite_positive_retourne_1(){
         int[] temperatures = {1, 2, 3};
         Thermometre thermometre = new Thermometre();
         int resultat = thermometre.thermometreEgalite(temperatures);
         assertEquals(1, resultat);
     }
 
+    /**
+     * On retourne le plus petit du tableau
+     */
     @Test
     public void thermometre_egalite_Positive_retourne_4(){
         int[] temperatures = {4, 5, 6, 7};
