df79437e6042c676c919a81290ebf283d33f0aa7
Anna RAACH <annagraph92@gmail.com>
2024-01-05 11:13:58 +0100

    docs(tp2): ajout de la documentation TU SalarieCdi 📝

--- src/main/java/fr/cda/demo/tp2/SalarieCdi.java
+++ src/main/java/fr/cda/demo/tp2/SalarieCdi.java
@@ -1,5 +1,9 @@
 package fr.cda.demo.tp2;
 
+/**
+ * Classe enfant de Salarie, la classe principale pour effectuer les test unitaires et les mocks
+ * étant donnée que la classe Salarie est une classe abstraite et donc impossible pour les instanciations
+ */
 public class SalarieCdi extends Salarie {
     public SalarieCdi(Salaire salaire, Identite identite) {
         super(salaire, identite);
