5b4c375cb36dd8378554a8463e56ea57b8cb2324
Anna RAACH <annagraph92@gmail.com>
2024-01-05 16:12:01 +0100

    test(tp3): test thermometre retourne 4 🧪

--- src/test/java/fr/cda/demo/tp3/thermometre/ThermometreTest.java
+++ src/test/java/fr/cda/demo/tp3/thermometre/ThermometreTest.java
@@ -10,11 +10,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ThermometreTest {
 
     @Test
-    public void thermometreEgalitePositive(){
+    public void thermometre_egalite_Positive_retourne_1(){
         int[] temperatures = {1, 2, 3};
         Thermometre thermometre = new Thermometre();
         int resultat = thermometre.thermometreEgalite(temperatures);
         assertEquals(1, resultat);
+    }
+
+    @Test
+    public void thermometre_egalite_Positive_retourne_4(){
+        int[] temperatures = {4, 5, 6, 7};
+        Thermometre thermometre = new Thermometre();
+        int resultat = thermometre.thermometreEgalite(temperatures);
+        assertEquals(4, resultat);
     }
 
 }
